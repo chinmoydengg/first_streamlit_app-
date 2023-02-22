bd6511044271a64011084e75bd714e76e42371c3
chinmoydengg <126054875+chinmoydengg@users.noreply.github.com>
2023-02-22 15:23:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 streamlit.title('My Parents Healthy Diner ')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('SomeTextSomeTextSomeTextSomeText')
+streamlit.text('SomeTextSomeTextSomeTextSomeTextSomeTextSomeText')
+streamlit.text('SomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeTextSomeText')
+
+
